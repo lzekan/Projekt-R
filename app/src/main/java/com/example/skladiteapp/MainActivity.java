27a250d1c194ba9700d11b3175986d7e0d19909e
Franjo Čuljak <fc53275@fer.hr>
2022-12-19 14:59:49 +0100

    navigation fully redone and functioning

--- app/src/main/java/com/example/skladiteapp/MainActivity.java
+++ app/src/main/java/com/example/skladiteapp/MainActivity.java
@@ -4,66 +4,34 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
     private DrawerLayout drawer;
+    private NavigationBarView navigationBarView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonTake = (Button) findViewById(R.id.buttonTake);
-        buttonTake.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, new TakeFragment())
-                        .commit();
-            }
-        });
+        navigationBarView = findViewById(R.id.bottomNavigationView);
 
-        Button buttonGet = (Button) findViewById(R.id.buttonGet);
-        buttonGet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, new GetFragment())
-                        .commit();
-            }
-        });
+        navigationBarView.setOnItemSelectedListener(this::onNavigationItemSelected);
+        navigationBarView.setSelectedItemId(R.id.iconIzdajAlt);
 
-        Button buttonRelocate = (Button) findViewById(R.id.buttonRelocate);
-        buttonRelocate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.fragment_container, new RelocateFragment())
-                        .commit();
-            }
-        });
-
-        Button buttonLogOut = (Button) findViewById(R.id.buttonLogout);
-        buttonLogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                createMessage("Želite li se odjaviti?", "logout");
-            }
-        });
-
-
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new TakeFragment()).commit();
     }
 
     private void createMessage(String text, String type) {
@@ -82,10 +50,39 @@ public class MainActivity extends AppCompatActivity {
         builder.setNegativeButton("Odustani", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                //nothing
+                navigationBarView.setSelectedItemId(R.id.iconIzdajAlt);
             }
         });
 
         builder.show();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.iconIzdajAlt:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new TakeFragment()).commit();
+                return true;
+
+            case R.id.iconZaprimiAlt:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new GetFragment()).commit();
+                return true;
+
+            case R.id.iconPremjestiAlt:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new RelocateFragment()).commit();
+                return true;
+
+            case R.id.iconStanjeAlt:
+                //getSupportFragmentManager().beginTransaction().replace(R.id.container, thirdFragment).commit();
+                return false;
+
+            case R.id.iconLogoutAlt:
+                createMessage("Želite li se odjaviti?", "logout");
+                return true;
+        }
+        return false;
     }
 }
