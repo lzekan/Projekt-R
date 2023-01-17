aaada2f7fc603b83c1f7a36b75671d916ed0ba30
lzekan <lz53052@fer.hr>
2023-01-17 20:53:41 +0100

    login preko web servisa

--- app/src/main/java/com/example/skladiteapp/LoginActivity.java
+++ app/src/main/java/com/example/skladiteapp/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.skladiteapp;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,13 +9,22 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -35,26 +46,56 @@ public class LoginActivity extends AppCompatActivity {
 
         //Login
         main.setOnClickListener(new View.OnClickListener() {
+            boolean wasFound = false;
+            EditText editTextUsername = (EditText) findViewById(R.id.inputUsername);
+            EditText editTextPassword = (EditText) findViewById(R.id.inputPassword);
+
             @Override
             public void onClick(View view) {
-                boolean wasFound = false;
-                Connection connect;
-                EditText editTextUsername = (EditText) findViewById(R.id.inputUsername);
-                EditText editTextPassword = (EditText) findViewById(R.id.inputPassword);
+                Thread thread = new Thread(() -> {
+                    URL url = null;
+                    try {
+                        url = new URL("http://192.168.62.166:8080/api/userByUsername?username="+editTextUsername.getText().toString());
+                        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                        urlConnection.setRequestMethod("GET");
+                        urlConnection.connect();
 
-                DatabaseHelperSQLite databaseHelperSQLite = new DatabaseHelperSQLite(LoginActivity.this);
-                Cursor data = databaseHelperSQLite.getData();
-                while(data.moveToNext()) {
-                    if (editTextUsername.getText().toString().equals(data.getString(1))
-                            && editTextPassword.getText().toString().equals(data.getString(2))) {
+                        StringBuilder sb = new StringBuilder();
 
-                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                        wasFound = true;
-                        break;
+                        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), UTF_8))){
+                            String str;
+                            while ((str = br.readLine()) != null) {
+                                sb.append(str);
+                            }
+                        }
+
+                        if(sb.toString().length() == 0) {
+                            createMessage("Neispravno korisničko ime ili lozinka.");
+                        }
+
+                        JSONObject obj = new JSONObject(sb.toString());
+
+                        if(editTextPassword.getText().toString().equals(obj.get("password"))){
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            wasFound = true;
+                        }
+
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
+
+                });
+
+                thread.start();
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
 
-                //error message
                 if(!wasFound) {
                     createMessage("Neispravno korisničko ime ili lozinka.");
                 }
